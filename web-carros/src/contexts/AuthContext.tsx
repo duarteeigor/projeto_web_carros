518804e972891a285c16c797f2b1dc34fd5be814
duarteeigor <duarteigor562@gmail.com>
2025-09-04 16:38:04 -0300

    feat: verificando status do usuario com onAuthStateChange

--- web-carros/src/contexts/AuthContext.tsx
+++ web-carros/src/contexts/AuthContext.tsx
@@ -1,26 +1,64 @@
-import { createContext, useState, } from "react";
+import { createContext, useEffect, useState, } from "react";
 import type { ReactNode } from "react";
+import { supabase } from "../services/supabaseClient";
 
-interface AuthProviderProps{
-    children : ReactNode
+interface AuthProviderProps {
+    children: ReactNode
 }
 
 type AuthContextData = {
     signed: boolean
+    loading: boolean
+    handleSaveInfoUser: ({id, email, name}: UserProps) => void
+    user: UserProps | null
 }
 
-interface UserProps{
-    uid: string,
+interface UserProps {
+    id: string,
     email: string | null,
     name: string | null
 }
 
 export const AuthContext = createContext({} as AuthContextData)
 
-function AuthProvider({children} : AuthProviderProps){
+function AuthProvider({ children }: AuthProviderProps) {
+
     const [user, setUser] = useState<UserProps | null>(null)
-    return(
-        <AuthContext.Provider value={{signed: !!user}}>
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        const {data: {subscription: {unsubscribe}}} = supabase.auth.onAuthStateChange((event, session) => {
+            if (session) {
+                setUser({
+                    id: session.user.id,
+                    email: session.user.user_metadata.email,
+                    name: session.user.user_metadata.name
+                })
+
+                setLoading(false)
+            } else {
+                console.log("Não existe usuario logado")
+                setUser(null)
+                setLoading(false)
+
+            }
+        })
+
+        return () =>{
+            unsubscribe()
+        }
+    }, [])
+
+    function handleSaveInfoUser({id, name, email}: UserProps){
+        setUser({
+            id,
+            name,
+            email
+        })
+    }
+
+    return (
+        <AuthContext.Provider value={{ signed: !!user, loading, handleSaveInfoUser, user}}>
             {children}
         </AuthContext.Provider>
     )
