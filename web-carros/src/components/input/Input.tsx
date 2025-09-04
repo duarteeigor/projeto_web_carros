1d4b406cbb99a025f868075b75053269f3413b1a
duarteeigor <duarteigor562@gmail.com>
2025-09-04 10:19:58 -0300

    feat: criaçao component input

---
+++ web-carros/src/components/input/Input.tsx
@@ -0,0 +1,25 @@
+import type { RegisterOptions, UseFormRegister } from "react-hook-form"
+
+interface InputProps{
+    type: string,
+    placeholder: string,
+    name: string,
+    register: UseFormRegister<any>
+    error?: string,
+    rules?: RegisterOptions
+}
+
+export function Input({type, placeholder, name, error, register, rules} : InputProps){
+    return(
+        <div>
+            <input
+                className="px-2 w-full outline-none h-11 rounded-md border-2 border-gray-200 mb-2"
+                placeholder={placeholder}
+                type={type}
+                {...register(name, rules)}
+                id={name}
+             />
+             {error && <p className="text-red-500">{error}</p>}
+        </div>
+    )
+}
